96f39e6ec7e71fd4c0c27dcba83eceecd88b20a1
alex32883 <alex32883@gmail.com>
2026-02-12 17:07:32 -0500

    Temporarily use simple function to test if it gets called

--- api/bot-simple.js
+++
@@ -1,8 +0,0 @@
-// Минимальная версия для тестирования
-module.exports = async (req, res) => {
-  console.log('=== SIMPLE FUNCTION CALLED ===');
-  console.log('Method:', req.method);
-  
-  // Сразу возвращаем 200 OK для всех запросов
-  res.status(200).json({ ok: true, message: 'Simple handler works' });
-};
